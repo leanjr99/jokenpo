32f0981b2b6674af160837d4ae6197b50572ce96
Leandro Jr <leandrojr70@gmail.com>
2021-04-07 15:46:42 -0300

    interface do app

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -13,6 +13,7 @@ class _JogoState extends State<Jogo> {
         title: Text("JokenPo"),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(
@@ -28,6 +29,29 @@ class _JogoState extends State<Jogo> {
               ),
             ),
           ),
+          Image.asset("images/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 32,
+              bottom: 16,
+            ),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize :20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset('images/pedra.png',height: 100,),
+              Image.asset('images/papel.png',height: 100,),
+              Image.asset('images/tesoura.png',height: 100,),
+            ],
+          )
         ],
       ),
     );
